731f4442b25079abbfd4b8a10bbf7ec64e008d63
root <carlos@back4app.com]>
2024-02-27 11:06:54 -0300

    second commit

--- containers_python_django/settings.py
+++ containers_python_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m+1z_2#1!&$$*72a%&z3j4!a=7^=!5@stt)-&v+9ray#1u+csa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0' ]
+ALLOWED_HOSTS = ['ctpydj-7xgojccz.b4a.run' ]
 
 
 
